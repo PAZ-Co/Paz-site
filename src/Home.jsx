d833435c8cd6656d8a9ed01945610503ac117659
PAZ-Co <precisionappraisalzone@gmail.com>
2025-09-16 18:12:40 -0400

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState, useEffect, useRef } from 'react';
-import fullLogo from './assets/logo.png';
+import logo from './assets/logo.png';
 import benji from './assets/benji-mascot.png';
 
 /**
